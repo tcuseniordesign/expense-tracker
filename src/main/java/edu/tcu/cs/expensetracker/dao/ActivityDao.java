c3e3bf40a7f67ad605d44f79732b56c27dc36e11
talmage123 <talmage123@gmail.com>
2022-04-23 11:18:06 -0500

    rebuild

--- src/main/java/edu/tcu/cs/expensetracker/dao/ActivityDao.java
+++
@@ -1,8 +0,0 @@
-package edu.tcu.cs.expensetracker.dao;
-
-import edu.tcu.cs.expensetracker.domain.Activity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ActivityDao extends JpaRepository<Activity, String> {
-    Activity findBycreatetime(String createTime);
-}
--- src/main/java/edu/tcu/cs/expensetracker/dao/UserDao.java
+++
@@ -1,9 +0,0 @@
-package edu.tcu.cs.expensetracker.dao;
-
-import edu.tcu.cs.expensetracker.domain.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-public interface UserDao extends JpaRepository<User, Integer> {
-    User findByUsername(String username);
-}
