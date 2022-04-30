9b502c3102ca36727a51d34ec245ef1c2fa2f91d
Aparajita Biswas <77810985+A-Biswas2@users.noreply.github.com>
2022-04-29 20:56:10 -0500

    Homework 4 Ques 2

---
+++ src/main/java/edu/tcu/cs/expensetracker/Dao/ActivityDao.java
@@ -0,0 +1,9 @@
+package edu.tcu.cs.expensetracker.Dao;
+import edu.tcu.cs.expensetracker.Domain.Activity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+
+public interface ActivityDao extends JpaRepository<Activity, Integer> {
+    List <Activity> findByDateBetween(String start, String end);
+
+}
---
+++ src/main/java/edu/tcu/cs/expensetracker/Service/ActivityService.java
@@ -0,0 +1,43 @@
+package edu.tcu.cs.expensetracker.Service;
+import edu.tcu.cs.expensetracker.Dao.ActivityDao;
+import edu.tcu.cs.expensetracker.Domain.Activity;
+import org.springframework.stereotype.Service;
+import javax.transaction.Transactional;
+import java.util.List;
+
+@Service
+@Transactional
+
+public class ActivityService {
+
+    private final ActivityDao activityDao;
+
+    public ActivityService(ActivityDao activityDao){
+        this.activityDao = activityDao;
+    }
+
+    public List<Activity> findAll() {
+        return activityDao.findAll();
+    }
+
+    public Activity findById(int activityId) {
+        return activityDao.findById(activityId).get();
+    }
+
+    public List<Activity> findByCriteria(String start, String end) {
+        return activityDao.findByDateBetween(start, end);
+    }
+
+    public void save(Activity newActivity) {
+        activityDao.save(newActivity);
+    }
+
+    public void update(int activityId, Activity updatedActivity) {
+        updatedActivity.setId(activityId);
+        activityDao.save(updatedActivity);
+    }
+
+    public void delete(int activityId) {
+        activityDao.deleteById(activityId);
+    }
+}
